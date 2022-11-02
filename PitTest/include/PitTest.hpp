00e52ba9399a58765d7d7d25c07db059ecd966c8
Peanutt42 <97802125+Peanutt42@users.noreply.github.com>
2022-11-02 18:27:37 +0100

    Improving Test creating workflow

--- PitTest/include/PitTest.hpp
+++ PitTest/include/PitTest.hpp
@@ -1,8 +1,9 @@
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <thread>
 
 #if _WIN32 || _WIN64
 #include <windows.h> // SetConsoleTextAttribute
@@ -46,97 +47,136 @@ namespace Pit::Test {
 	}
 #endif     // !!! UNTESTED !!!
 
-	FORCEINLINE void PitTests() {
+
+
+	void RunAllPitTests() {
 		SetConsoleColor(CONSOLE_GREEN);
 		std::cout << "[PitTest]\n";
 		int testsFailed = 0;
 
 		for (auto& testEntry : _PitTestsRegister::Tests) {
-			SetConsoleColor(CONSOLE_WHITE);
-			std::cout << testEntry.name << "...";
+			SetConsoleColor(CONSOLE_GREEN);
+			std::cout << "\n[" << testEntry.name << "]\n";
 			bool result = testEntry.testFunc();
-			if (result) {
-				SetConsoleColor(CONSOLE_GREEN);
-				std::cout << " [OK]\n";
-			}
-			else {
-				SetConsoleColor(CONSOLE_RED);
-				std::cout << " [FAILED]\n";
+			if (!result)
 				testsFailed++;
-			}
-			SetConsoleColor(CONSOLE_WHITE);
 		}
 
 		if (testsFailed == 0) {
 			SetConsoleColor(CONSOLE_GREEN);
-			std::cout << "ALL TESTS PASSED :>\n";
+			std::cout << "\nALL TESTS PASSED :>\n";
 		}
 		else {
 			SetConsoleColor(CONSOLE_RED);
-			std::cout << testsFailed << " TEST" << (testsFailed > 1 ? "S" : "") << " FAILED :[\n";
+			std::cout << "\n" << testsFailed << " TEST" << (testsFailed > 1 ? "S" : "") << " FAILED >:[\n";
 		}
 		SetConsoleColor(CONSOLE_WHITE);
 	}
+}
 
-#define PIT_TEST_BASETEST(name, testFunc)	\
-	struct TEST_##name {											\
-		static std::string GetTestName() { return #name; }			\
-		static std::function<bool()> GetTestFunc() {				\
-			return []() { testFunc };					\
-		}															\
-		static bool s_registered;									\
-	};																\
-	bool TEST_##name::s_registered = _PitTestsRegister::Register<TEST_##name>()
+#define PIT_TEST_BASETEST(name, testFunc)						\
+	struct TEST_##name {										\
+		static std::string GetTestName() { return #name; }		\
+		static std::function<bool()> GetTestFunc() {			\
+			return []() { testFunc };							\
+		}														\
+		static bool s_registered;								\
+	};															\
+	bool TEST_##name::s_registered = Pit::Test::_PitTestsRegister::Register<TEST_##name>()
 
-#define PIT_TEST_EQUALS(name, val1, val2)	\
-	PIT_TEST_BASETEST(name, {return val1 == val2;})
+#define PIT_TEST(name, func)									\
+	PIT_TEST_BASETEST(name, { func })
 
-#define PIT_TEST_DIFFRENCE(name, val1, val2)	\
-	PIT_TEST_BASETEST(name, {return val1 != val2;})
+#define PIT_TEST_EQUALS(name, val1, val2)						\
+	Pit::Test::SetConsoleColor(CONSOLE_WHITE);					\
+	std::cout << #name;											\
+	if (val1 == val2) {											\
+		Pit::Test::SetConsoleColor(CONSOLE_GREEN);				\
+		std::cout << " [OK]\n";									\
+	}															\
+	else {														\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [FAILED]\n";								\
+	}
 
-#define PIT_TEST_TRUE(name, result)		\
-	PIT_TEST_BASETEST(name, {return result;})
+#define PIT_TEST_DIFFRENCE(name, val1, val2)					\
+	Pit::Test::SetConsoleColor(CONSOLE_WHITE);					\
+	std::cout << #name;											\
+	if (val1 != val2) {											\
+		Pit::Test::SetConsoleColor(CONSOLE_GREEN);				\
+		std::cout << " [OK]\n";									\
+	}															\
+	else {														\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [FAILED]\n";								\
+	}
 
-#define PIT_TEST_TRY_FUNC(name, func)	\
-	PIT_TEST_BASETEST(name, {										\
-		bool failed = false;										\
-		try {														\
-			func();													\
-		}															\
-		catch (const std::exception& e) {							\
-			failed = true;											\
-			SetConsoleColor(CONSOLE_RED);							\
-			std::cout << " [EXCEPTION]: " << e.what();				\
-			SetConsoleColor(CONSOLE_WHITE);							\
-		}															\
-		catch (...) {												\
-			failed = true;											\
-			SetConsoleColor(CONSOLE_RED);							\
-			std::cout << " [EXCEPTION TRIGGERED]";					\
-			SetConsoleColor(CONSOLE_WHITE);							\
-		}															\
-		return !failed;												\
-	})
+#define PIT_TEST_TRUE(name, result)								\
+	Pit::Test::SetConsoleColor(CONSOLE_WHITE);					\
+	std::cout << #name;											\
+	if (result) {												\
+		Pit::Test::SetConsoleColor(CONSOLE_GREEN);				\
+		std::cout << " [OK]\n";									\
+	}															\
+	else {														\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [FAILED]\n";								\
+	}
 
-#define PIT_TEST_TRY_LAMDA(name, lamda)	\
-	PIT_TEST_BASETEST(name, {										\
-		bool failed = false;										\
-		try {														\
-			{ lamda	}												\
-		}															\
-		catch (const std::exception& e) {							\
-			failed = true;											\
-			SetConsoleColor(CONSOLE_RED);							\
-			std::cout << " [EXCEPTION]: " << e.what();				\
-			SetConsoleColor(CONSOLE_WHITE);							\
-		}															\
-		catch (...) {												\
-			failed = true;											\
-			SetConsoleColor(CONSOLE_RED);							\
-			std::cout << " [EXCEPTION TRIGGERED]";					\
-			SetConsoleColor(CONSOLE_WHITE);							\
-		}															\
-		return !failed;												\
-	})
+#define PIT_TEST_TRY_FUNC(name, func)							\
+	{															\
+	Pit::Test::SetConsoleColor(CONSOLE_WHITE);					\
+	std::cout << #name;											\
+	try {														\
+		func();													\
+	}															\
+	catch (const std::exception& e) {							\
+		failed = true;											\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [EXCEPTION]: " << e.what();				\
+		Pit::Test::SetConsoleColor(CONSOLE_WHITE);				\
+	}															\
+	catch (...) {												\
+		failed = true;											\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [EXCEPTION TRIGGERED]";					\
+		Pit::Test::SetConsoleColor(CONSOLE_WHITE);				\
+	}															\
+	if (failed) return false;									\
+	else {														\
+		Pit::Test::SetConsoleColor(CONSOLE_GREEN);				\
+		std::cout << " [OK]\n";									\
+	}															\
+	}
 
+#define PIT_TEST_TRY_LAMDA(name, lamda)							\
+	{															\
+	bool failed = false;										\
+	Pit::Test::SetConsoleColor(CONSOLE_WHITE);					\
+	std::cout << #name;											\
+	try {														\
+		{ lamda	}												\
+	}															\
+	catch (const std::exception& e) {							\
+		failed = true;											\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [EXCEPTION]: " << e.what();				\
+		Pit::Test::SetConsoleColor(CONSOLE_WHITE);				\
+	}															\
+	catch (...) {												\
+		failed = true;											\
+		Pit::Test::SetConsoleColor(CONSOLE_RED);				\
+		std::cout << " [EXCEPTION TRIGGERED]";					\
+		Pit::Test::SetConsoleColor(CONSOLE_WHITE);				\
+	}															\
+	if (failed) return false;									\
+	else {														\
+		Pit::Test::SetConsoleColor(CONSOLE_GREEN);				\
+		std::cout << " [OK]\n";									\
+	}															\
+	}
+
+int main() {
+	Pit::Test::RunAllPitTests();
+	std::cin.get();
 }
--- TestApplication/src/main.cpp
+++ TestApplication/src/main.cpp
@@ -1,12 +1,17 @@
 #include <PitTest.hpp>
 
-using namespace Pit::Test;
+PIT_TEST(SomeTestGroup, 
+	PIT_TEST_TRY_LAMDA(SomeShortTest, Sleep(400););
+	PIT_TEST_TRY_LAMDA(SomeLongTest, Sleep(2000););
+	PIT_TEST_TRY_LAMDA(SomeSmallTest, Sleep(10););
+	PIT_TEST_TRUE(SomeEasyTest, 1 + 1 == 2);
+	return true;
+);
 
-PIT_TEST_TRY_LAMDA(SomeShortTest, Sleep(400););
+PIT_TEST(SomeTestGroup2,
+		 PIT_TEST_TRY_LAMDA(SomeShortTest, Sleep(400););
 PIT_TEST_TRY_LAMDA(SomeLongTest, Sleep(2000););
 PIT_TEST_TRY_LAMDA(SomeSmallTest, Sleep(10););
 PIT_TEST_TRUE(SomeEasyTest, 1 + 1 == 2);
-
-int main() {
-	PitTests();
-}
+return true;
+);
